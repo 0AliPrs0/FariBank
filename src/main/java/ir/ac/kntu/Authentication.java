12e2aa7cc5fc8c1085a6cec0610c45c8e0e13e01
aliprs <alip13840505@gmail.com>
2024-05-15 19:54:34 +0330

    add Authentication

---
+++ src/main/java/ir/ac/kntu/Authentication.java
@@ -0,0 +1,42 @@
+package ir.ac.kntu;
+
+public class Authentication extends User{
+    private String supportOpinion;
+    private boolean isAcceptRequest;
+    private boolean isCheckSupport;
+
+    public Authentication(){
+
+    }
+
+    public Authentication(String firstName, String lastName, String phoneNumber, String id, String password, String supportOpinion, boolean isAcceptRequest, boolean isCheckSupport) {
+        super(firstName, lastName, phoneNumber, id, password);
+        this.supportOpinion = supportOpinion;
+        this.isAcceptRequest = isAcceptRequest;
+        this.isCheckSupport = isCheckSupport;
+    }
+
+    public String getSupportOpinion() {
+        return supportOpinion;
+    }
+
+    public void setSupportOpinion(String supportOpinion) {
+        this.supportOpinion = supportOpinion;
+    }
+
+    public boolean isAcceptRequest() {
+        return isAcceptRequest;
+    }
+
+    public void setAcceptRequest(boolean acceptRequest) {
+        isAcceptRequest = acceptRequest;
+    }
+
+    public boolean isCheckSupport() {
+        return isCheckSupport;
+    }
+
+    public void setCheckSupport(boolean checkSupport) {
+        isCheckSupport = checkSupport;
+    }
+}
--- src/main/java/ir/ac/kntu/Main.java
+++ src/main/java/ir/ac/kntu/Main.java
@@ -6,22 +6,22 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-//        ArrayList<User> user = new ArrayList<>();
-//        ArrayList<Request> requests = new ArrayList<>();
+        ArrayList<UserAccount> user = new ArrayList<>();
+        ArrayList<Authentication> authentications = new ArrayList<>();
         RoleMenu role;
 
         do {
             Menus.getInstance().printTheMainMenu();
             role = Menus.getInstance().getOptionMainMenu();
-            handleTheMainMenu(role);
+            handleTheMainMenu(role, user, authentications);
         } while (role != RoleMenu.EXIT);
 
         ScannerWrapper.getInstance().close();
     }
 
-    public static void handleTheMainMenu(RoleMenu roles){
+    public static void handleTheMainMenu(RoleMenu roles, UserMenu user, Authentication authentication){
         switch (roles) {
-            case USER -> handleUserRoles(user, requests);
+            case USER -> handleUserRoles(user, authentication);
             case SUPPORT -> handleSupportRoles();
             case EXIT -> System.out.println("Exiting the program ...");
             default -> System.out.println("Invalid Input!");
@@ -30,5 +30,9 @@ public class Main {
 
     public static void handleUserRoles(){
         UserHandler.implementTheUserMenu();
+    }
+
+    public static void handleSupportRoles(){
+
     }
 }
