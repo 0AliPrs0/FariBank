c8ead265b7987cbd16a1585eaa67abdfc1bd33d2
aliprs <alip13840505@gmail.com>
2024-05-20 01:12:46 +0330

    add ChargeAccount

---
+++ src/main/java/ir/ac/kntu/ChargeAccount.java
@@ -0,0 +1,38 @@
+package ir.ac.kntu;
+
+public class ChargeAccount {
+    private int chargeAmount;
+    private String dateOfChargeAccount;
+
+    public ChargeAccount() {
+    }
+
+    public ChargeAccount(int chargeAmount, String dateOfChargeAccount) {
+        this.chargeAmount = chargeAmount;
+        this.dateOfChargeAccount = dateOfChargeAccount;
+    }
+
+    public int getChargeAmount() {
+        return chargeAmount;
+    }
+
+    public void setChargeAmount(int chargeAmount) {
+        this.chargeAmount = chargeAmount;
+    }
+
+    public String getDateOfChargeAccount() {
+        return dateOfChargeAccount;
+    }
+
+    public void setDateOfChargeAccount(String dateOfChargeAccount) {
+        this.dateOfChargeAccount = dateOfChargeAccount;
+    }
+
+    @Override
+    public String toString() {
+        return "ChargeAccount{" +
+                "chargeAmount=" + chargeAmount +
+                ", dateOfChargeAccount='" + dateOfChargeAccount + '\'' +
+                '}';
+    }
+}
