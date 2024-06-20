e956108a070bc6daea8716003e4b28e9b92f433b
aliprs <alip13840505@gmail.com>
2024-06-20 23:58:02 +0330

    Final Version

--- src/main/java/ir/ac/kntu/user/info/RemainingFund.java
+++ src/main/java/ir/ac/kntu/user/info/RemainingFund.java
@@ -3,7 +3,6 @@ package ir.ac.kntu.user.info;
 import ir.ac.kntu.main.enums.FundType;
 
 public class RemainingFund extends CapitalFund {
-
     public RemainingFund() {
         super("remaining fund", FundType.REMAINING_FUND, 0);
     }
